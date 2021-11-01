004fc3030ae45a0637d4a32ac95e6176f47a08b8
Gulam Ali <gulam2275@gmail.com>
2021-11-01 11:31:08 +0530

    Initial Commit

---
+++ LoginPageSWIFTUI/LoginPageSWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageSWIFTUIApp.swift
+//  LoginPageSWIFTUI
+//
+//  Created by Gulam Ali on 01/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageSWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
